46afc7073a8f655b9275a6508692aa9fb4f9c153
Benny <SAU21649@spengergasse.at>
2021-10-30 21:15:26 +0200

    add bootstrap add javascript add jquery

---
+++ erlebnisgastronomie/resources/views/layouts/header.blade.php
@@ -0,0 +1,35 @@
+!DOCTYPE html>
+<html
+    lang="de">
+<head>
+    <meta
+        charset="utf-8">
+    <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0">
+    <meta
+        name="description"
+        content="">
+    <meta
+        name="eigenständigaufgebaut"
+        content="">
+    <title>
+        Home
+        |
+        Cyber
+        Security</title>
+    <link
+        href="{{asset('css/bootstrap.min.css')}}"
+        rel="stylesheet">
+    <link
+        href="{{asset('css/font-awesome.min.css')}}"
+        rel="stylesheet">
+
+</head>
+
+
+
+
+
+<script src="{{asset('js/bootstrap.min.js')}}"></script>
+<script src="{{asset('js/jquery.js')}}"></script>
