817b84ec3feb0bb025d5ee57f598aabae11077ff
Benny <SAU21649@spengergasse.at>
2021-11-06 16:10:07 +0100

    add homepage change in web.php add login route to  "home" in web.php

--- erlebnisgastronomie/routes/web.php
+++ erlebnisgastronomie/routes/web.php
@@ -14,12 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('products');
 });
 
 
-Route::get('/products', function () {
-    return view('products');
+Route::get('/home', function () {
+    return view('home');
 });
 
 
