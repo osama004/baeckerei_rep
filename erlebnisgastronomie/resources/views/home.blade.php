172e0cbea07da7c43eae4c5dca206393a9520b0a
Benny <SAU21649@spengergasse.at>
2022-01-06 13:00:28 +0100

    --change footer details

--- erlebnisgastronomie/resources/views/home.blade.php
+++ erlebnisgastronomie/resources/views/home.blade.php
@@ -490,18 +490,18 @@
             </div>
             <div class="col-md-5 mb-5">
                 <div class="mb-5">
-                    <h3>Opening Hours</h3>
+                    <h3>Öffnungszeiten</h3>
                     <p><strong class="d-block font-weight-normal text-black">Sunday-Thursday</strong> 5AM - 10PM</p>
                 </div>
 
             </div>
             <div class="col-md-3 mb-5">
-                <h3>Contact Info</h3>
+                <h3>Kontaktdaten</h3>
                 <ul class="list-unstyled footer-link">
                     <li class="d-block">
-                        <span class="d-block text-black">Address:</span>
-                        <span>34 Street Name, City Name Here, United States</span></li>
-                    <li class="d-block"><span class="d-block text-black">Phone:</span><span>+1 242 4942 290</span></li>
+                        <span class="d-block text-black">Adresse:</span>
+                        <span>Jägerstraße 62, 1200 Wien Österreich </span></li>
+                    <li class="d-block"><span class="d-block text-black">Telefon:</span><span>+1 242 4942 290</span></li>
                     <li class="d-block"><span class="d-block text-black">Email:</span><span>info@yourdomain.com</span></li>
                 </ul>
             </div>
