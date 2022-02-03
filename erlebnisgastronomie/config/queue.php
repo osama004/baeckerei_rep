e9c9c40a260a0832e4076d348e83d2040bc5c9ad
osama <mad21662@spengergasse.at>
2022-02-03 14:28:04 +0100

    falied_querey to be saved in the table failed_jobs

--- erlebnisgastronomie/config/queue.php
+++ erlebnisgastronomie/config/queue.php
@@ -87,7 +87,7 @@ return [
     'failed' => [
         'driver' => env('QUEUE_FAILED_DRIVER', 'database-uuids'),
         'database' => env('DB_CONNECTION', 'mysql'),
-        'table' => 'failedjob',
+        'table' => 'failed_jobs',
     ],
 
 ];
