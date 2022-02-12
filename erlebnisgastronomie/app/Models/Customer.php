5d10b599c64fb7a0e50bdf200a1585e089e0b79e
osama <mad21662@spengergasse.at>
2022-02-12 23:02:43 +0100

    add phone and address

--- erlebnisgastronomie/app/Models/Customer.php
+++ erlebnisgastronomie/app/Models/Customer.php
@@ -27,7 +27,8 @@ class Customer extends Authenticatable implements MustVerifyEmail
         'lastname',
         'email',
         'password',
-        //'address',
+        'address',
+        'phone',
     ];
 
 
--- erlebnisgastronomie/resources/views/auth/register.blade.php
+++ erlebnisgastronomie/resources/views/auth/register.blade.php
@@ -235,7 +235,7 @@
                                 </div>
 
                                 <div class="form-group row">
-                                    <label for="address" class="col-md-4 col-form-label text-md-right">{{ __('address') }}*</label>
+                                    <label for="address" class="col-md-4 col-form-label text-md-right">{{ __('Adresse') }}*</label>
 
                                     <div class="col-md-6">
                                         <input id="address" type="text" class="form-control @error('address') is-invalid @enderror" name="address" value="{{ old('address') }}" required autocomplete="address">
@@ -249,7 +249,7 @@
                                 </div>
 
                                 <div class="form-group row">
-                                    <label for="phone" class="col-md-4 col-form-label text-md-right">{{ __('phone') }}*</label>
+                                    <label for="phone" class="col-md-4 col-form-label text-md-right">{{ __('Telefone') }}*</label>
 
                                     <div class="col-md-6">
                                         <input id="phone" type="text" class="form-control @error('phone') is-invalid @enderror" name="phone" value="{{ old('phone') }}" required autocomplete="phone">
