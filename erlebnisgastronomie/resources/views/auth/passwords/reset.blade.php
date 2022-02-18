593c1d19567b79909ac45c222c44e88d4127c1f3
Benny <SAU21649@spengergasse.at>
2022-02-18 10:01:09 +0100

    --restyle reset.blade.php

--- erlebnisgastronomie/resources/views/auth/passwords/reset.blade.php
+++ erlebnisgastronomie/resources/views/auth/passwords/reset.blade.php
@@ -1,65 +1,219 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Bäckquem</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700,800|DM+Serif+Display:400,400i&display=swap" rel="stylesheet">
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('password.update') }}">
-                        @csrf
+    <link rel="shortcut icon" href="{{asset('ftco-32x32.png')}}">
 
-                        <input type="hidden" name="token" value="{{ $token }}">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700,800" rel="stylesheet">
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+    <link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
+    <link rel="stylesheet" href="{{asset('css/animate.css')}}">
+    <link rel="stylesheet" href="{{asset('css/owl.carousel.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/aos.css')}}">
+    <link href="{{asset('/css/all.min.css')}}" rel="stylesheet">
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
+    <link rel="stylesheet" href="{{asset('css/magnific-popup.css')}}">
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
+
+    <link rel="stylesheet" href="{{asset('fonts/ionicons/css/ionicons.min.css')}}">
+    <link rel="stylesheet" href="{{asset('fonts/fontawesome/css/font-awesome.min.css')}}">
+    <link rel="stylesheet" href="{{asset('fonts/flaticon/font/flaticon.css')}}">
+
+    <!-- Theme Style -->
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+</head>
+
+<body>
+<header role="banner">
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="/">Bäckquem</a>
+            <a href="https://www.twitter.com" class="p-2" target="_blank" rel="noopener noreferrer"><span class="fa fa-twitter"></span></a>
+            <a href="https://www.facebook.com" class="p-2" target="_blank" rel="noopener noreferrer"><span class="fa fa-facebook"></span></a>
+            <a href="https://www.instagram.com" class="p-2" target="_blank" rel="noopener noreferrer"><span class="fa fa-instagram"></span></a>
+
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample05" aria-controls="navbarsExample05" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarsExample05">
+                <ul class="navbar-nav ml-auto pl-lg-5 pl-0">
+
+                    <li class="nav-item">
+                        <a class="nav-link active" href="/">Home</a>
+                    </li>
+
+                    <div class="divider"></div>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{route('kaffee&products')}}">Kaffee & Produkte</a>
+                    </li>
+
+                    <div class="divider"></div>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{route('weeklyCart')}}">Wochenkarte</a>
+                    </li>
+
+                    <div class="divider"></div>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{route('regionalProducts')}}">Regionale Produkte</a>
+                    </li>
+
+                    <div class="divider"></div>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="app">App</a>
+                    </li>
+
+                    <div class="divider"></div>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{route('contact')}}">Kontakt</a>
+                    </li>
+
+                    <div class="divider"></div>
+
+                    @guest
+                        @if (Route::has('login'))
+                            @if (Route::has('register'))
+                                <li class="nav-item dropdown">
+                                    <a  style="text-transform:capitalize"  id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                        <i class="fas fa-user"></i>
+                                    </a>
+
+                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="{{route('/login')}}" >
+                                            Login
+                                        </a>
+
+                                        <a class="dropdown-item" href="/register" >
+                                            Registrieren
+                                        </a>
+                                    </div>
+                                </li>
+                                <div class="divider"></div>
+
+                            @endif
+                        @endif
+
+                    @else
+                        <li class="nav-item dropdown">
+                            <a  style="text-transform:capitalize"  id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                <i class="fas fa-user"></i>
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{route('/userprofile')}}" >
+                                    Mein Profil
+                                </a>
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                    {{ __('Ausloggen') }}
+                                </a>
+                                @if($userData -> isAdmin())
+                                    <a class="dropdown-item" href="{{route('adminDisplayProducts')}}">
+                                        Admin Dashboard
+                                    </a>
+                                @endif
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+                    @endguest
+
+                    <div class="divider"></div>
+
+                    <li class="nav-item" href="{{route('shoppingcart')}}">
+                        <a class="nav-link" href="{{route('shoppingcart')}}">
+                            <i class="fas fa-shopping-cart">
+                                @if(Session::has('cart'))
+                                    <span class="cart-with-numbers">
+                                        {{ Session::get('cart')->totalQuantity }}
+                                   </span>
+                                @endif
+                            </i>
+                        </a>
+
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
+<!-- END header -->
+<div class="slider-wrap">
+    <div class="slider-item" style="background-image: url('/images/new_filler_1.jpg');">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <div class="card">
+                        <div class="card-header">{{ __('Reset Password') }}</div>
+
+                        <div class="card-body">
+                            <form method="POST" action="{{ route('password.update') }}">
+                                @csrf
+
+                                <input type="hidden" name="token" value="{{ $token }}">
+
+                                <div class="form-group row">
+                                    <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+
+                                    <div class="col-md-6">
+                                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
+
+                                        @error('email')
+                                        <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
-                                @enderror
-                            </div>
-                        </div>
+                                        @enderror
+                                    </div>
+                                </div>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                                <div class="form-group row">
+                                    <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                                    <div class="col-md-6">
+                                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
 
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
+                                        @error('password')
+                                        <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
-                                @enderror
-                            </div>
-                        </div>
+                                        @enderror
+                                    </div>
+                                </div>
 
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+                                <div class="form-group row">
+                                    <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
 
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
+                                    <div class="col-md-6">
+                                        <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                                    </div>
+                                </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Reset Password') }}
-                                </button>
-                            </div>
+                                <div class="form-group row mb-0">
+                                    <div class="col-md-6 offset-md-4">
+                                        <button type="submit" class="btn btn-primary">
+                                            {{ __('Reset Password') }}
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
                         </div>
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
-@endsection
+
+
